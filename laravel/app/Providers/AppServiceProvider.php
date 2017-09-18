cbca82d1c5f3c5e0822154e71db0c52115d323cf
RodrigoDot <rodrigodot@outlook.com>
2017-09-18 18:46:04 -0300

    alterado o idioma do FAKER para pt-br, a anterior nao funcionou, AGORA esta tudo perfeito

--- laravel/app/Providers/AppServiceProvider.php
+++ laravel/app/Providers/AppServiceProvider.php
@@ -23,6 +23,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if($this->app->environment() !== 'production') {
+          $this->app->singleton(\Faker\Generator::class, function() {
+            return \Faker\Factory::create(env('FAKER_LANGUAGE'));
+          });
+        }
     }
 }
